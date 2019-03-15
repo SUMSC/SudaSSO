cd33e899a1e538903835a3568182ce3825405c8d
LionTao <taojiachun980831@163.com>
2019-03-15 19:56:40 +0800

    add some feature

--- server.py
+++ server.py
@@ -1,23 +1,41 @@
+from gevent import monkey
+monkey.patch_all()
 import falcon
 from app.sso import checkMyauth
+import ujson as json
+with open('.hash.conf','r') as fp:
+  passwd=json.load(fp)['pass']
 keys=set(['id_tag','secret','timestamp'])
-passwd='test'
 # checkMyauth(id_tag: str, secret: str, clienttime: str, passwd: str) -> bool
 class sudaauth(object):
   def on_get(self, req, resp):
     if keys & req.params.keys():
+      print(req.params)
       id_tag=req.params['id_tag']
       secret=req.params['secret']
       timestamp=req.params['timestamp']
+      name=req.params['name']
+      usertype=req.params['usertype']
+      print("id_tag:{}\nsecret:{}\ntimestamp:{}".format(id_tag,secret,timestamp))
       res=checkMyauth(id_tag=id_tag,secret=secret,clienttime=timestamp,passwd=passwd)
+      print()
       resp.status = falcon.HTTP_200
-      resp.body="Success "+str(res)
+      if res:
+        resp.body=json.dumps({"status":"ok","data":{"id":id_tag,"name":name,"usertype":usertype}})
+      else:
+        resp.bosy=json.dumps({"status":"error","data":"Timed Out"})
     else:
       resp.status = falcon.HTTP_404
-      resp.body='Not Allowed'
+      resp.body='Logouted\n'
 
+class logout(object):
+  def on_get(self,req,resp):
+    resp.status = falcon.HTTP_200
+    resp.body='Logouted\n'
     
 api = application = falcon.API()
 
 auth=sudaauth()
-api.add_route('/', auth)
+logouter=logout()
+api.add_route('/login', auth)
+api.add_route('/logout',logouter)
